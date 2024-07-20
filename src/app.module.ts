c4a9f53dab4883e84e777793a192c363d8acebad
eleo romario <eleo.romario@gmail.com>
2024-07-20 09:09:10 -0500

    chore: Add ConfigModule and ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ClientsModule } from './clients/clients.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from './config/config.module';
 @Module({
-  imports: [AuthModule, ClientsModule, PrismaModule],
+  imports: [AuthModule, ClientsModule, PrismaModule, ConfigModule],
   controllers: [],
   providers: []
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,29 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ClientsService } from 'src/clients/clients.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, ClientsService, PrismaService]
+  providers: [AuthService, ClientsService, PrismaService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        console.log(process.env.JWT_SECRET);
+        return {
+          secret: configService.jwtSecret,
+          signOptions: {
+            expiresIn: configService.jwtExpiresIn
+          }
+        };
+      }
+    })
+  ]
 })
 export class AuthModule {}
