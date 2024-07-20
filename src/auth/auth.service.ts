981601cbb16bd04e90ea9604281c06679afaaf52
eleo romario <eleo.romario@gmail.com>
2024-07-20 08:38:16 -0500

    feat: Update authentication service to use UnauthorizedException

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginClientDto } from './dto/login-client.dto';
 import { ClientsService } from 'src/clients/clients.service';
 import { Client } from '@prisma/client';
@@ -14,11 +14,11 @@ export class AuthService {
     const userDB = await this.clientService.findOneEmail(email);
 
     if (!userDB) {
-      throw new BadRequestException('User with this email does not exist');
+      throw new UnauthorizedException('User with this email does not exist');
     }
 
     if (!bcrypt.compareSync(password, userDB.password)) {
-      throw new BadRequestException('Invalid password');
+      throw new UnauthorizedException('Invalid password');
     }
 
     delete userDB.password;
